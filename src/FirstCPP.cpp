c8969787e5fa9f5873a9c0687aee4e91d303672c
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2017-01-30 19:25:29 -0700

    base commit

---
+++ src/FirstCPP.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : FirstCPP.cpp
+// Author      : Cody
+// Version     :
+// Copyright   : no copyright
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Greetings to all mammals!" << endl; // prints Greetings to all mammals!
+	int stop;
+	cin >> stop;
+	return 0;
+}
