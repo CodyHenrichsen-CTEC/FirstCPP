fda8c82a03770c3391ac87bcb455b9983578041b
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2017-01-30 21:30:22 -0700

    Added code from mac.

---
+++ src/Controller.cpp
@@ -0,0 +1,13 @@
+/*
+ * Controller.cpp
+ *
+ *  Created on: Jan 30, 2017
+ *      Author: cody.henrichsen
+ */
+
+void Controller :: start()
+{
+
+}
+
+
---
+++ src/Controller.hpp
@@ -0,0 +1,19 @@
+/*
+ * Controller.hpp
+ *
+ *  Created on: Jan 30, 2017
+ *      Author: cody.henrichsen
+ */
+
+#ifndef CONTROLLER_HPP_
+#define CONTROLLER_HPP_
+
+class Controller
+{
+public:
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_HPP_ */
